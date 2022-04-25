1576f18a00a47d116d1965d38cf75cb5777b3de6
Larissa-TH <larissatozetti@hotmail.com>
2022-04-25 14:29:32 -0300

    Avaliação técnica 3

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Prova3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] medicamentos = {"omelprazol", "paracetamol", "dipirona", "amoxicilina", "doril", "buscopan" };
+            
+            
+            
+
+            Console.WriteLine("Qual remedio quer pesquisar: ");
+           string pesquisa =Console.ReadLine().ToLower();
+            int index = Array.IndexOf(medicamentos, pesquisa);
+            Console.WriteLine($"O remédio pesquisado foi: {pesquisa}, e está na posição: {index}");
+
+            Console.WriteLine("Total de elementos no Array: "+medicamentos.Length);
+            Console.WriteLine(" ");
+            Array.Sort(medicamentos);
+
+            Console.WriteLine("Em ordem alfabetica os remédio são: ");
+            foreach(string medicamento in medicamentos)
+            {
+                Console.WriteLine(medicamento);
+            }
+               
+        }
+    }
+}
